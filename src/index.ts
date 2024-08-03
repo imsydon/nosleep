cd8a95e60ad6dd3ec4f9395647f0c0ed9d7d7f36
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2024-08-03 19:01:27 +0700

    add wake on lan

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,36 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { macAddress } from "../macAddress";
 
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+	return c.text("Hello Hono!");
 });
 
 app.get("/invoke", (c) => {
-  console.log("invoked!");
-  return c.text("invoked!");
+	console.log("invoked!");
+	return c.text("invoked!");
+});
+
+app.get("/wake", (c) => {
+	var wol = require("wake_on_lan");
+
+	wol.wake(macAddress, function (error) {
+		if (error) {
+			return c.text("error");
+		} else {
+			return c.text("wol!");
+		}
+	});
+
+	return c.text("huh");
 });
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
 
 serve({
-  fetch: app.fetch,
-  port,
+	fetch: app.fetch,
+	port,
 });
